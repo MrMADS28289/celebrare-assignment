eb753a36f9b0bc4c3caddae7e446520b5461602a
ADSM <yesform2018@gmail.com>
2023-05-20 17:49:45 +0600

    project 90% completed

--- js/app.js
+++ js/app.js
@@ -13,16 +13,37 @@ function getImg(event) {
         aspectRatio: 0,
         viewMode: 0
     });
+
+    /*** geting croped image ***/
+    document.getElementById('crop-btn').addEventListener('click', function () {
+        let cropedImg = cropper.getCroppedCanvas().toDataURL("image/png");
+        modal.style.display = "none";
+        modal2.style.display = "block";
+        document.getElementById('use-image').src = cropedImg;
+
+        const output = document.getElementById('output');
+        document.getElementById('use-btn').addEventListener('click', function () {
+            output.src = cropedImg;
+            modal2.style.display = "none";
+            output.style.display = "block";
+        });
+    });
+
 }
 
 imputImage?.addEventListener('change', getImg)
 
 /*** Modal ***/
 let modal = document.getElementById("crop-modal");
+let modal2 = document.getElementById("frame-modal");
 let close = document.getElementsByClassName("close")[0];
+let close2 = document.getElementsByClassName("close2")[0];
 
 close.onclick = function () {
     modal.style.display = "none";
+}
+close2.onclick = function () {
+    modal2.style.display = "none";
 }
 
 window.onclick = function (event) {
@@ -30,3 +51,4 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
